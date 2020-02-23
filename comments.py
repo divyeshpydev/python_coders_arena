ac3ea6c5a1eac014d4e73b54a5755e0926aade3e
divyeshpydev <61373771+divyeshpydev@users.noreply.github.com>
2020-02-23 13:11:03 +0530

    Day 1

---
+++ comments.py
@@ -0,0 +1,20 @@
+name='divyesh'
+age=23 #jkfdblabf 
+#there is name and age present
+age=marks=kt=23
+
+
+''' 
+qualification="post graduate"
+qualification="post graduate"
+qualification="post graduate"
+'''
+
+print("hello my name is ",name," My age is ",age)
+#  String formatting - concatination
+full_name = "hello My Name is "+name+" my age is "+str(age) # Explicit type casting
+
+formated_name = f"hello My name is {name} my age is {age}" # No type casting
+lol = "hello mt name is %s and i am not a terrorist %d"%(name,age) 
+print(full_name)
+print(formated_name)
---
+++ datatypes.py
@@ -0,0 +1,10 @@
+age = 21 # int
+name = 'divyesh'
+some_list = ['m3','mp','tcs','spcc','spa']
+student={
+    'name':'divyesh',
+    'age':'22',
+    'subject': some_list,
+}
+# print(student['subject'][2])
+print(name[::-1])
